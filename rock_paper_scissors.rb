794b544f1d3b77637bd2feb70f7c72b45ed0f086
Ryan Bales <ryankbales@gmail.com>
2014-10-01 12:30:00 -0700

    working version of game with some minor features added

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -7,9 +7,9 @@ def best_weapon(weapon_one, weapon_two)
 
   if weapon_one == "rock"
     if weapon_two == "paper"
-      win_lose_tie = "lose"
-    elsif  weapon_two == "scissors"
       win_lose_tie = "win"
+    elsif  weapon_two == "scissors"
+      win_lose_tie = "lose"
     else
       win_lose_tie = "tie"
     end
@@ -17,9 +17,9 @@ def best_weapon(weapon_one, weapon_two)
 
   if weapon_one == "paper"
     if weapon_two == "rock"
-      win_lose_tie = "win"
-    elsif  weapon_two == "scissors"
       win_lose_tie = "lose"
+    elsif  weapon_two == "scissors"
+      win_lose_tie = "win"
     else
       win_lose_tie = "tie"
     end
@@ -27,9 +27,9 @@ def best_weapon(weapon_one, weapon_two)
 
   if weapon_one == "scissors"
     if weapon_two == "rock"
-      win_lose_tie = "lose"
-    elsif  weapon_two == "paper"
       win_lose_tie = "win"
+    elsif  weapon_two == "paper"
+      win_lose_tie = "lose"
     else
       win_lose_tie = "tie"
     end
@@ -37,10 +37,10 @@ def best_weapon(weapon_one, weapon_two)
   return win_lose_tie
 end
 
-puts = "Rock, Paper, Scissors!"
+puts "Rock, Paper, Scissors!"
 
 loop do
-  
+
   begin
     puts "Choose your weapon! (r)ock, (p)aper or (s)cissors"
     human_input = gets.chomp.downcase.to_sym
@@ -49,6 +49,8 @@ loop do
   machine_input  = WEAPONS.keys.shuffle.first
   machine_input = WEAPONS[machine_input]
   result = best_weapon(machine_input, human_input)
+  puts "The computer picked #{machine_input}"
+  puts "You picked #{human_input}"
   if result == "win"
     puts "You win!"
   elsif result == "lose"
@@ -56,6 +58,6 @@ loop do
   else
     puts "It's a tie, try again!"
   end
-  
-  
+
+
 end
